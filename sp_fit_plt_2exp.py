784b0f3b4ef3658ca0576deb845497d590cf850e
Evgenii Shevchenko <shvgn@protonmail.ch>
2015-02-17 21:08:05 +0300

    sp_fit_plt_2exp.py still doesn't work

--- sp_fit_plt_2exp.py
+++ sp_fit_plt_2exp.py
@@ -12,34 +12,47 @@ import os
 
 import matplotlib.pyplot as pl
 import numpy as np
-from scipy.optimize import curve_fit
+from scipy.odr import odrpack as odr
 
 import spectrum as sp
 
 
-
-# TIME_PERIOD = 13.158  # nanoseconds
-# ITERATIONS_NUMBER = 300
-# INDEX_LENGTH_AFTER_MAX = 10
-# INDEX_OF_GOOD_CURVE_START = 250
 CONST_BOLZMANN = 8.6173324e-5  # eV / K
 
 
-def fitfun(t, t0, c1, c2, e1, e2):
+def coreexp(t0, p, t):
     """
-    Double exponent thermal PL quenching
-    I = fitfun(t, t0, c1, c2, e1, e2) =
+    Exponential thermal PL quenching
+    p = [c, e]
+    I = coreexp(t0, p, t) =
+      =  (1 + c * np.exp(-e / (CONST_BOLZMANN * (t - t0))) ) ** -1
+    """
+    a = p[0]
+    c = p[1]
+    e = p[2]
+    return a * (1 + c * np.exp(-e / (CONST_BOLZMANN * (t - t0)))) ** -1
+
+
+def coreexp2(t0, p, t):
+    """
+    Double exponential thermal PL quenching
+    p = [c1, c2, e1, e2]
+    I = corefunc(t0, p, t) =
       =  (1 + c1 * np.exp(-e1 / (CONST_BOLZMANN * (t - t0))) + c2 * np.exp(-e2 / (CONST_BOLZMANN * (t - t0)))) ** -1
     """
+    c1 = p[0]
+    c2 = p[1]
+    e1 = p[2]
+    e2 = p[3]
     return (1 + c1 * np.exp(-e1 / (CONST_BOLZMANN * (t - t0))) + c2 * np.exp(-e2 / (CONST_BOLZMANN * (t - t0)))) ** -1
 
 
-def fitfun_f(t0):
+def fitfunc(t0, func):
     """
-    Double exponent thermal PL quenching closure with defined t and t0
-    I = fitfun_f(t, t0) -> fitfun(c1, c2, e1, e2)
+    Single or double exponent thermal PL quenching closure with defined t0
+    I = fitfunc(t0, corefunc) -> corefunc(p, t) @ t0
     """
-    return lambda t, c1, c2, e1, e2: fitfun(t, t0, c1, c2, e1, e2)
+    return lambda p, t: func(t0, p, t)
 
 
 if len(sys.argv) < 2:
@@ -59,7 +72,8 @@ legend = []
 
 print("Filename\t\tc1\t\tc2\t\te1\t\te2")
 
-initial_params = (180., 5., 0.04, 0.01)
+# initial_params = [100., 5., 0.05, 0.01]
+initial_params = [1.1, 80., 0.092]
 for spec in data:
 
     maxidx = np.argmax(spec.y)
@@ -68,36 +82,58 @@ for spec in data:
         maxidx = 15
         spec = spec / spec.y[maxidx]
 
-    t0 = spec.x[maxidx]
+    # t0 = spec.x[maxidx]
+    t0 = 0.0
 
-    xfit = spec.x[maxidx:len(spec)]
-    yfit = spec.x[maxidx:len(spec)]
+    xdata = spec.x[maxidx:len(spec)]
+    ydata = spec.x[maxidx:len(spec)]
+    func = coreexp
+    model = odr.Model(fitfunc(t0, func))
+    data = odr.Data(xdata, ydata)
 
-    try:
-        params, conv = curve_fit(fitfun_f(t0), xfit, yfit, p0=initial_params)
-        print(conv)
-    except RuntimeError:
-        # print("Couldn't find optimal parameters.")
-        print(spec.headers['filepath'], "\t-\t-\t-\t-")
-        continue
+    fitting = odr.ODR(data, model, beta0=initial_params)
+    fitting.set_job(fit_type=2)  # 2 corresponds to least squares
+    fitres = fitting.run()
 
-    # Parameters are in order [c1, c2, e1, e2]
-    fmt = "\t%f\t%f\t%f\t%f"
-    print(spec.headers['filepath'], fmt % tuple(params))
-    # print(spec.headers['filepath'], params)
+    fitres.pprint()
+    # print(fitres.beta)
+    # print(fitres.cov_beta)
+    # print(fitres.sd_beta)
 
-    # Fitted sum of stretched exponents
-    pl.plot(1 / xfit, fitfun(xfit, t0, *params))
-    # pl.semilogy(1 / xfit, fitfun(xfit, t0, *params))
-    legend.append("Fitting [c1=%.2f, c2=%.2f, e1=%.2f, e2=%.2f]" % tuple(params))
+
+    # try:
+    # params, conv = curve_fit(fitfunc(t0), xdata, ydata, p0=initial_params)
+    # print(conv)
+    # except RuntimeError:
+    # # print("Couldn't find optimal parameters.")
+    # print(spec.headers['filepath'], "\t-\t-\t-\t-")
+    # continue
+    #
+    # # Parameters are in order [c1, c2, e1, e2]
+    # fmt = "\t%f\t%f\t%f\t%f"
+    # print(spec.headers['filepath'], fmt % tuple(params))
+    # # print(spec.headers['filepath'], params)
+    #
+    # # Fitted sum of stretched exponents
+    # pl.plot(1 / xdata, corefunc(xdata, t0, *params))
+    # # pl.semilogy(1 / xfit, fitfun(xfit, t0, *params))
+    # legend.append("Fitting [c1=%.2f, c2=%.2f, e1=%.2f, e2=%.2f]" % tuple(params))
+    #
+
+    # Fitted params
+    pl.plot(xdata, func(t0, fitres.beta, xdata))
+    # pl.semilogy(1 / xfit, fitfun(xfit, t0, *initial_params))
+    legend.append("Fitted " + str(tuple(fitres.beta)))
+    # legend.append("Fitted [c1=%.2f, c2=%.2f, e1=%.2f, e2=%.2f]" % tuple(fitres.beta))
 
     # Initial params
-    pl.plot(xfit, fitfun(xfit, t0, *initial_params))
+    pl.plot(xdata, func(t0, initial_params, xdata))
     # pl.semilogy(1 / xfit, fitfun(xfit, t0, *initial_params))
-    legend.append("Initial [c1=%.2f, c2=%.2f, e1=%.2f, e2=%.2f]" % tuple(initial_params))
+    legend.append("Initial " + str(tuple(initial_params)))
+    # legend.append("Initial [c1=%.2f, c2=%.2f, e1=%.2f, e2=%.2f]" % tuple(initial_params))
 
     # Original data
-    pl.plot(spec.x, spec.y)
+    pl.plot(spec.x, spec.y, 'o')
     # pl.semilogy(1 / spec.x, spec.y)
     legend.append(os.path.basename(spec.headers['filepath']))
 
