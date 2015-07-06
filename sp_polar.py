973e99f701c3e202c0ee5d386a6c84d6dca64372
Evgenii Shevchenko <shvgn@protonmail.ch>
2015-07-06 13:09:33 +0300

    Polar plot script

---
+++ sp_polar.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# TODO make both nm and eV scales in one axes or two subplots
+
+import sys
+import os
+
+import numpy as np
+import matplotlib.pyplot as pl
+
+import spectrum as sp
+
+MAX_LEGEND_ENTRY_LEN = 30
+
+if len(sys.argv) == 1:
+    print("usage: {0} datafile1 [datafile2 ...]".format(
+        os.path.basename(sys.argv[0])))
+    sys.exit(0)
+
+data = []
+for arg in sys.argv[1:]:
+    if not (os.path.exists(arg) and os.path.isfile(arg)):
+        print("Warning! Cannot open file <" + arg + ">. Skipping.")
+        continue
+    data.append(sp.spectrum_from_file(arg))
+
+pl.figure()
+legend = []
+for spdata in data:
+    legend_item = os.path.basename(spdata.headers['filepath'])
+    legend_item = legend_item.replace("ev-", "")
+    if len(legend_item) > MAX_LEGEND_ENTRY_LEN:
+        # Only first ten characters. We unlikely need more.
+        legend.append(legend_item[0:MAX_LEGEND_ENTRY_LEN - 3] + "...")
+    else:
+        legend.append(legend_item)
+    pl.polar(spdata.x * np.pi / 180, spdata.y, label=legend_item)
+pl.grid(True)
+pl.legend(legend)
+pl.show()
